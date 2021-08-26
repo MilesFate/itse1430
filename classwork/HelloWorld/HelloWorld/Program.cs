7754f9b1afc717adb7b060b4abc823da7729e312
t2223239_s <t2223239_s@NEITD-110812>
2021-08-25 21:40:58 -0500

    changes

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿/*
+ * File Header
+ * Luisalberto Castaneda
+ * ITSE 1430 Fall 2021
+ */
+using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
-            xyz
             Console.WriteLine("Hello World!");
         }
     }
