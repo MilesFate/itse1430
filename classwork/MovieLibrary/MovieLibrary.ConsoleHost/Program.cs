60f084ba264c22a7bcc1a0b36e6dbb9ef56fce69
t2223239_s <t2223239_s@NEITD-110812>
2021-09-20 21:37:11 -0500

    objects

---
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,198 @@
+﻿// Luisalberto Castaneda
+// ITSE 1450
+// Movie Libary
+using System;
+
+namespace MovieLibrary.ConsoleHost
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            bool done = false;
+
+            do
+            {
+                char choice = GetInput();
+                /*if (choice == 'Q')
+                    done = HandleQuit();
+                else if (choice == 'A')
+                    AddMovie();
+                else if (choice == 'V')
+                    ViewMovie();
+                else if (choice == 'D')
+                    DeleteMovie();
+                else
+                    Console.WriteLine("Unknown Option");
+                */
+                switch(choice)
+                {
+                    case 'Q': done = HandleQuit(); break;
+                    case 'A': AddMovie(); break;
+                    case 'V': ViewMovie(); break;
+                    case 'D': DeleteMovie(); break;
+                    default: DisplayError("Unknown Option"); break;
+                };
+                
+            } while (!done);
+        }
+
+        static Movie movie = new Movie();
+
+        private static void DeleteMovie ()
+        {
+            // Confirm
+            if (!ReadBoolean("Are you sure (Y/N)? "))
+                return;
+
+            // TODO: Delete Movie
+            movie.title = null;
+        }
+
+        static void ViewMovie()
+        {
+            // TODO: what if they haven't added one yet?
+            if (String.IsNullOrEmpty(movie.title))
+            {
+                Console.WriteLine("No movie available");
+                return;
+            }
+                // TODO: Formatting
+            Console.WriteLine($"{movie.title}({movie.releaseYear})");
+            Console.WriteLine($"RunTime : {movie.runLength} Mins");
+            Console.WriteLine($"MPAA rating {movie.rating}");
+            Console.WriteLine($"Classic? {movie.isClassic}");
+            Console.WriteLine(movie.description);
+        }
+
+        private static void AddMovie ()
+        {
+            movie.title = ReadString("Enter the movie title: ",true); // requires
+            movie.description = ReadString("Enter the optional description: ",false); // optional
+
+            movie.runLength = ReadInt32("Enter Run Length (in Minutes): ",0); // optional, in minutes, >=0
+            movie.releaseYear = ReadInt32("Enter the release year (min 1900): ",1900); // 1900+
+
+            // double reviewRating; // optional, 0.0 to 5.0
+            movie.rating= ReadString("Enter the MPAA rating: ",false); // optional, MPAA (not enforced)
+            movie.isClassic = ReadBoolean("Is this a classic (Y/N)? "); // optional
+        }
+
+        private static int ReadInt32 ( string message, int minimumvalue )
+        {
+            Console.Write(message);
+            do
+            {
+                // string input = Console.ReadLine();
+                var input = Console.ReadLine();
+
+                // int result = Int32.Parse(input);  // crashes program, not good for input
+                // int result;
+
+                // if string parsed AND result at least min value
+                // if (Int32.TryParse(input, out result))
+                //   if (result >= minimumvalue)
+                if (Int32.TryParse(input, out var result) && result >= minimumvalue)
+                    return result;
+
+                DisplayError("The Value must be an intergal value >= " + minimumvalue);
+            } while (true);
+            // return -1;
+        }
+
+        private static string ReadString ( string message, bool required )
+        {
+            Console.Write(message);
+
+            do
+            {
+                string input = Console.ReadLine().Trim();
+
+                if(!String.IsNullOrEmpty(input) || !required)
+                    return input;
+                DisplayError("Value is required");
+            } while (true);          
+        }
+
+        static void DisplayError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static bool HandleQuit ()
+        {
+            // Display a confirmation
+            if(ReadBoolean("Are you sure you want to quit (Y/N)?"))
+            // return results
+                return true;
+            return false;
+        }
+
+        private static bool ReadBoolean ( string message )
+        {
+            Console.Write(message);
+            do
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Y)
+                    return true;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
+ 
+            } while (true);
+
+            // not needed anymore
+            // return false;
+        }
+
+        static char GetInput ()
+        {
+            Console.WriteLine("Movie Libary");
+            //Console.WriteLine("------------");
+            Console.WriteLine("".PadLeft(15,'-'));
+            Console.WriteLine("A) dd");
+            Console.WriteLine("V) iew");
+            Console.WriteLine("D) elete");
+            Console.WriteLine("Q) uit");
+
+            while (true)
+            {
+                string input = Console.ReadLine().Trim();
+               /* if (input == "Q")
+                    return 'Q';
+                else if (input == "A")
+                    return 'A';
+                else if (input == "V")
+                    return 'V';
+                else if (input == "D")
+                    return 'D';
+               */
+               switch(input.ToUpper())
+                {
+                    // No fallthough , unless case statement empty
+                    // must end in break or return
+                    //case "c":
+                    case "C": return 'B';
+
+                    //case "q":
+                    case "Q": return 'Q';
+
+                    //case "a":
+                    case "A": return 'A';
+
+                    //case "v":
+                    case "V": return 'V';
+
+                    //case "d":
+                    case "D": return 'D';
+                    // default:;
+                };
+
+                DisplayError("invalid input");
+            }
+            // return default(char);
+        }
+    }
+}
---
+++ classwork/MovieLibrary/MovieLibrary/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MovieLibrary
+{
+    // naming rules for class
+    // 1. Pascal cased
+    // 2. Never prefix with T, C or anything else
+    // 3. Noun - because the represent an object/entity in your system
+
+    /// <summary> Represents a movie. </summary>
+
+    public class Movie
+    {
+        // Fields
+        // 1. Always camel cased, TODO:for now
+        // 2. Should NEVER be public
+        // 3. Always zero initalized or can default
+        // 4. Cannot initalize to another field's value
+
+        public string title;
+        public string description;
+        public int runLength;
+        public int releaseYear;
+        public double reviewRating;
+        public string rating;
+        public bool isClassic;
+    }
+}
