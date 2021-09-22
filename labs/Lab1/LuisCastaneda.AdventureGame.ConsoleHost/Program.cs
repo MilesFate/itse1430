c235d4760154380ea39aab69850e811de5009b2c
Anton Fate <bossbot213@gmail.com>
2021-09-21 23:35:46 -0500

    finished

--- labs/Lab1/LuisCastaneda.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/LuisCastaneda.AdventureGame.ConsoleHost/Program.cs
@@ -11,7 +11,7 @@ namespace LuisCastaneda.AdventureGame.ConsoleHost
     internal class Program
     {
         static string s_direction;        
-        public static int s_roomNumber = 1;
+        public static int s_roomNumber = 5;
 
         private static void Main()
         {
@@ -39,7 +39,6 @@ namespace LuisCastaneda.AdventureGame.ConsoleHost
         {
             switch (s_roomNumber)
             {
-                case 0: HandleError("invalid input"); break;
                 case 1: room1(); break;
                 case 2: room2(); break;
                 case 3: room3(); break;
@@ -99,8 +98,6 @@ namespace LuisCastaneda.AdventureGame.ConsoleHost
                     default : HandleError("Invalid Input"); break;
                 }
             } while (true);
-            //return s_roomNumber = 1;
-
             
         }
 
@@ -187,100 +184,125 @@ namespace LuisCastaneda.AdventureGame.ConsoleHost
         {
             Console.WriteLine("ITSE 1430, Luisalberto Castaneda, Adventure Game, Fall 2020");
             Console.WriteLine("-------------------------------------------------------------");
-            Console.WriteLine();
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("As you wake from from a deep slumber you find that you are in a stange room. ");
+            Console.Write("Getting off the table you were lying on you keep an eye on the shadows in the room, noticing that they seem... strange. ");
+            Console.Write("Exting from this room leads you to an ally more fit to called a slum then the marvel it was sold as. ");
+            Console.Write("In this rundown crumbling ally you notice a clean, mainatined door that seems more unnerving then your current state. ");
+            Console.Write("Entering this door you find that it has swallowed you into an abyss. ");
+            Console.WriteLine("Regaining sight, you scan the sourndings only to find that you are in a drim room with a door on each wall.");
+            Console.ResetColor();
         }
+        
 
         private static void room1 ()
         {
             Console.WriteLine("1");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("After going through the door you feel the atmosphere become more relaxed hearing Kaizo music, feeling like you can lower your gaurd. ");
+            Console.Write("You aproach a bar counter and are handed a water by the bar tender with him softly saying \"Water is the best drink you could offer someon.\". ");
+            Console.Write("Looking around you see a man holding a sigh with \"IDK\" printed on it while he whispers \"schwelp\". ");
+            Console.WriteLine("Knowing you need to find an exit, you relunctely force yourself to leave this safehaven. ");
+            Console.ResetColor();
         }
 
         private static void room2 ()
         {
             Console.WriteLine("2");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Entering the room there was an instant change in atmosphere, you then hear \"Each room comes in isalation of the others meaning no two rooms are the same.\". ");
+            Console.Write("Looking around it seems as you are in some kind of kids birthday party. ");
+            Console.Write("The more you look the more try and remember if you had ever been there but nothing ever seems to fit, like it was a burning memory. ");
+            Console.WriteLine("Notcing the shadows in the room seem to be too big to fit in the room, you decide you should leave. ");
+            Console.ResetColor();
         }
 
         private static void room3 ()
         {
             Console.WriteLine("3");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Stepping into the room you are met with the sand of a desert, the room seems so vast that you nearly miss the edges of the room. ");
+            Console.Write("In the middle of this artificial landscape you see a statue hidden. ");
+            Console.Write("On this statue is read \" Here lies Bright, the Ruler of Rulers.\". ");
+            Console.WriteLine("Notcing the shadows in the room seem to be too big to fit in the room, you decide you should leave. ");
+            Console.ResetColor();
         }
 
         private static void room4 ()
         {
             Console.WriteLine("4");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Entering the room there was an instant change in atmosphere, you then hear \"Each room comes in isalation of the others meaning no two rooms are the same.\". ");
+            Console.Write("Seeing the stage so empty made you think of the worlds cruitly, thinking that they desvere a perfromance of a lifetime. ");
+            Console.Write("As the curtains raise you see that every seat is full, everyone waiting for an oncore of the ages. ");
+            Console.Write("After the posetion of your body by the virtuose, the sence of perfection fills your thoughts. ");
+            Console.Write("The curtain lowers and you regain your senses feeling like you gave the audiance what they came to see. ");
+            Console.WriteLine("Feeling there is nothing more to do, you feel as if its time to go to another room. ");
+            Console.ResetColor();
         }
 
         private static void room5 ()
         {
             Console.WriteLine("5");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Upon further examnation of the room it seems as though many have passed into this room before your arrival.");
+            Console.Write("With the walls looking centries old in some spots and in other seeming to be from a time yet to come it is clear that this new enviroment holds many secrets. ");
+            Console.Write("The flicker of the candle catches the eye making you wonder \"Who lit the candle?\". ");
+            Console.WriteLine("Regardless of the candles presence, the shadows seem to be too big to fit in the room, they seem to be growing the longer you stay. ");
+            Console.ResetColor();
         }
-
+        
         private static void room6 ()
         {
             Console.WriteLine("6");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Entering the room there was an instant change in atmosphere, you then hear \"Each room comes in isalation of the others meaning no two rooms are the same.\". ");
+            Console.Write("With the room being lit up by purple lights it gave off a certain feeling. ");
+            Console.Write("You are then approached by an indian man who then spends hours telling you why should set up a virtual machine for ever website you visit. ");
+            Console.WriteLine("Feeling there is nothing more to do, you feel as if its time to go to another room. ");
+            Console.ResetColor();
         }
 
         private static void room7 ()
         {
             Console.WriteLine("7");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Entering the room you notice there are three doors, two are freely acceable while the third is blocked by a wall. ");
+            Console.Write("Examining the wall you see it is made of pure diamond, you start to punch it to try and get passed it. ");
+            Console.Write("After puching at the wall for a few hours you think to yourself \"it would take an eternity to get through this wall.\". ");
+            Console.WriteLine("Notcing the shadows in the room seem to be too big to fit in the room, you decide you should leave. ");
+            Console.ResetColor();
         }
 
         private static void room8 ()
         {
             Console.WriteLine("8");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Entering the room there was an instant change in atmosphere, you then hear \"Each room comes in isalation of the others meaning no two rooms are the same.\". ");
+            Console.Write(" \"Your eyes deceive you\", ");
+            Console.Write(" \"An illusion fools you all\", ");
+            Console.WriteLine(" \"Was just a mirror\" ");
+            Console.WriteLine("Feeling there is nothing more to do, you feel as if its time to go to another room. ");
+            Console.ResetColor();
         }
 
         private static void room9 ()
         {
             Console.WriteLine("9");
-            Console.Write("Sentence 1");
-            Console.Write("Sentence 2");
-            Console.Write("Sentence 3");
-            Console.WriteLine("Sentence 4");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Upon entering you notice an imideate change. ");
+            Console.Write("It's as if time and space have both stop existing the way you have come to understand them. ");
+            Console.Write("After regaing your composure after what was the greatest shift a body can have, you notice a man leaning agaisnt the wall. ");
+            Console.WriteLine("Uproaching the man he smiles and says \"What's up broski, I'm Vlad and this is my time!\", you awkardly wave and look for an exit. ");
+            Console.ResetColor();
         }
 
         private static void help ()
         {
-            Console.WriteLine("Valid Move inputs : Right | Left | Forward | Backward");
+            Console.WriteLine("Move    :: Right | Left | Up | Down");
             Console.WriteLine("Examine :: ReShows what room your in");
-            Console.WriteLine("Help :: Get Help");
-            Console.WriteLine("Quit :: Quit the Program");
+            Console.WriteLine("Help    :: Get Help");
+            Console.WriteLine("Quit    :: Quit the Program");
         }
     }
 }
