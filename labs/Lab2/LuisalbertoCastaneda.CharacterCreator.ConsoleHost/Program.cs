8cc5c0d6ec919fd291a4790a315e5a9d8c6838de
Anton Fate <bossbot213@gmail.com>
2021-10-05 00:53:08 -0500

    did stufz

---
+++ labs/Lab2/LuisalbertoCastaneda.CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,233 @@
+﻿/*
+ * Luisalberto Castaneda
+ * ITSE-1430
+ * Lab2 - Character Creator
+ */
+using System;
+
+namespace LuisalbertoCastaneda.CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine("ITSE 1430 Character Creator");
+            Console.WriteLine("Luisalberto Castaneda ITSE-1430 Fall 2021");
+            var s1 = "";
+            Console.WriteLine(s1.PadLeft(50, '-'));
+            var exit = false;
+
+            do
+            {
+                DisplayMainMenu();
+                int choice = GetIntInput("Enter your choice: ");
+                switch (choice)
+                {
+                    case 0 : exit = HandleConfirmation("Are you sure you want to quit?"); break;
+                    case 1 : AddCharacter(); break;
+                    case 2 : ViewCharacter(); break;
+                    case 3 : EditCharacter(); break;
+                    case 4 : DeleteCharacter(); break;
+                    default : HandleError("Value not on the menu."); break;
+                }
+            } while (!exit);
+        }
+
+        static Character s_character;
+
+        static void DisplayMainMenu ()
+        {
+            Console.WriteLine("0 : Quit");
+            Console.WriteLine("1 : Add Character");
+            Console.WriteLine("2 : View Character");
+            Console.WriteLine("3 : Edit Character");
+            Console.WriteLine("4 : Delete Character");
+        }
+
+        static void AddCharacter ()
+        {
+            Character newCharacter = new Character();
+            newCharacter.Name = GetStringInput("Enter the name for the new character: ", true);
+            newCharacter.Profession = GetProfession();
+            newCharacter.Race = GetRace();
+            newCharacter.Biography = GetStringInput("Enter the biography for the new character (optional): ", false);
+            newCharacter.Strength = GetCharacterAttributeNumber("Enter the character's strength: ");
+            newCharacter.Intelligence = GetCharacterAttributeNumber("Enter the character's intelligence: ");
+            newCharacter.Agility = GetCharacterAttributeNumber("Enter the character's agility: ");
+            newCharacter.Constitution = GetCharacterAttributeNumber("Enter the character's constitution: ");
+            newCharacter.Charisma = GetCharacterAttributeNumber("Enter the character's charisma: ");
+            s_character = newCharacter;
+        }
+
+        static void ViewCharacter ()
+        {
+            if (s_character != null)
+            {
+                Console.WriteLine("---- Character Information ----");
+                Console.WriteLine();
+                Console.WriteLine($"Name: {s_character.Name}");
+                Console.WriteLine($"Profession: {s_character.Profession}");
+                Console.WriteLine($"Race: {s_character.Race}");
+                Console.WriteLine($"Biography: {s_character.Biography}");
+                Console.WriteLine($"Strength: {s_character.Strength}");
+                Console.WriteLine($"Intelligence: {s_character.Intelligence}");
+                Console.WriteLine($"Agility: {s_character.Agility}");
+                Console.WriteLine($"Constitution: {s_character.Constitution}");
+                Console.WriteLine($"Charisma: {s_character.Charisma}");
+                Console.WriteLine();
+            } else
+                HandleError("There is no character created.");
+        }
+
+        static void EditCharacter ()
+        {
+            if (s_character == null)
+                AddCharacter();
+
+            Console.WriteLine($"Name: {s_character.Name}");
+            if (HandleConfirmation("Do you want to change the character's name? (Y/N)"))
+                s_character.Name = GetStringInput("Enter the new name for the character: ", true);
+
+            Console.WriteLine($"Profession: {s_character.Profession}");
+            if (HandleConfirmation("Do you want to change the character's profession? (Y/N)"))
+                s_character.Profession = GetProfession();
+
+            Console.WriteLine($"Race: {s_character.Race}");
+            if (HandleConfirmation("Do you want to change the character's race? (Y/N)"))
+                s_character.Race = GetRace();
+
+            Console.WriteLine($"Biography: {s_character.Biography}");
+            if (HandleConfirmation("Do you want to change the character's biography? (Y/N)"))
+                s_character.Biography = GetStringInput("Enter the new biography for the character: ", false);
+
+            Console.WriteLine($"Strength: {s_character.Strength}");
+            if (HandleConfirmation("Do you want to change the character's strength? (Y/N)"))
+                s_character.Strength = GetIntInput("Enter the character's strength: ");
+
+            Console.WriteLine($"Intelligence: {s_character.Intelligence}");
+            if (HandleConfirmation("Do you want to change the character's intelligence? (Y/N"))
+                s_character.Intelligence = GetIntInput("Enter the character's intelligence: ");
+
+            Console.WriteLine($"Agility: {s_character.Agility}");
+            if (HandleConfirmation("Do you want to change the character's Agility? (Y/N)"))
+                s_character.Agility = GetIntInput("Enter the character's agility: ");
+
+            Console.WriteLine($"Constitution: {s_character.Constitution}");
+            if (HandleConfirmation("Do you want to change the character's Constitution? (Y/N)"))
+                s_character.Constitution = GetIntInput("Enter the character's constitution: ");
+
+            Console.WriteLine($"Charisma: {s_character.Charisma}");
+            if (HandleConfirmation("Do you want to change the character's Charisma?"))
+                s_character.Charisma = GetIntInput("Enter the character's charisma: ");
+
+            ViewCharacter();
+        }
+
+        static void DeleteCharacter ()
+        {
+            if (s_character != null)
+            {
+                if (HandleConfirmation("Are you sure you want to delete the character? (Y/N)"))
+                {
+                    s_character = null;
+                    Console.WriteLine("Character deleted.");
+                } else
+                    Console.WriteLine("Character not deleted.");
+            } else
+                HandleError("There is no character to delete.");
+        }
+
+        static string GetProfession ()
+        {
+            do
+            {
+                string input = GetStringInput("Enter the profession for the character: ", true);
+
+                if (input == "SNIPER" || input == "HUNTER" || input == "ASSASSIN" || input == "MAGE" || input == "CHAPO")
+                    return input;
+                else
+                    HandleError("You Must Input : Sniper, Hunter, Assassin, Mage, Chapo");
+
+            } while (true);
+        }
+
+        static string GetRace ()
+        {
+            do
+            {
+                string input = GetStringInput("Enter the race for the character: ", true);
+
+                if (input == "ZENO" || input == "HUMAN" || input == "RACCOON" || input == "ROBOT" || input == "MONSTER")
+                    return input;
+                else
+                    HandleError("You Must Input : Zeno, Human, Raccoon, Robot, Monster");
+
+            } while (true);
+        }
+
+        static string GetStringInput ( string message, bool required )
+        {
+            do
+            {
+                Console.Write(message);
+                string input = Console.ReadLine().Trim().ToUpper();
+                if (!String.IsNullOrEmpty(input) || !required)
+                    return input;
+                else
+                    HandleError("Input is required.");
+
+            } while (true);
+        }
+
+        static int GetIntInput ( string message )
+        {
+            do
+            {
+                Console.Write(message);
+                var input = Console.ReadLine();
+                if (Int32.TryParse(input, out var result))
+                    return result;
+                HandleError("Invalid Input");
+
+            } while (true);
+        }
+
+        static int GetCharacterAttributeNumber ( string message )
+        {
+            do
+            {
+                int value = GetIntInput(message);
+
+                if (value <= Character.maximumValue && value >= Character.minimumValue)
+                    return value;
+                else
+                    HandleError("Value not in range. (1 - 100)");
+
+            } while (true);
+        }
+
+        static bool HandleConfirmation ( string message )
+        {
+            do
+            {
+                Console.WriteLine(message);
+                ConsoleKeyInfo answer = Console.ReadKey(true);
+                switch (answer.Key)
+                {
+                    case ConsoleKey.Y: return true;
+                    case ConsoleKey.N: return false;
+                    default: HandleError("Invalid input."); break;
+                }
+
+            } while (true);
+        }
+
+
+        static void HandleError ( string Message )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ labs/Lab2/LuisalbertoCastaneda.CharacterCreator/Character.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Luisalberto Castaneda
+ * ITSE-1430
+ * Lab2 - Character Creator
+ */
+using System;
+
+namespace LuisalbertoCastaneda.CharacterCreator
+{
+    /// <summary> Represents a Character </summary>
+    public class Character
+    {
+        private string _name;
+        private string _profession;
+        private string _race;
+        private string _biography;
+
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value?.Trim(); }
+        }
+
+        public string Profession
+        {
+            get { return _profession ?? ""; }
+            set { _profession = value?.Trim(); }
+        }
+
+        public string Race
+        {
+            get { return _race ?? ""; }
+            set { _race = value?.Trim(); }
+        }
+
+        public string Biography
+        {
+            get { return _biography ?? ""; }
+            set { _biography = value?.Trim(); }
+        }
+
+        public const int maximumValue = 100, minimumValue = 1;
+
+        public int Strength { get; set; } = minimumValue;
+
+        public int Intelligence { get; set; } = minimumValue;
+
+        public int Agility { get; set; } = minimumValue;
+
+        public int Constitution { get; set; } = minimumValue;
+
+        public int Charisma { get; set; } = minimumValue;
+    }
+}
