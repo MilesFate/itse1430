a171cc0f90cd811b4fedb4ce48fc0ee18bf4f835
t2223239_s <t2223239_s@NEITD-110812>
2021-10-06 21:14:49 -0500

    ui learning

---
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MovieLibrary.WinHost
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Additional init here
+            // Runs at design Time as well - be careful
+        }
+    }
+}
--- classwork/MovieLibrary/MovieLibrary/Movie.cs
+++ classwork/MovieLibrary/MovieLibrary/Movie.cs
@@ -2,105 +2,167 @@
 
 namespace MovieLibrary
 {
-    // naming rules for class
+    //Naming rules for class
     // 1. Pascal cased
     // 2. Never prefix with T, C or anything else
-    // 3. Noun - because the represent an object/entity in your system
+    // 3. Noun - because they represent an object/entity in your system
 
-    /// <summary> Represents a movie. </summary>
-
+    /// <summary>Represents a movie.</summary>
     public class Movie
     {
-        public int Id { get; private set; }
+        #region Constructors (demo only)
+
+        //Default constructor
+        public Movie ()
+        { }
+
+        public Movie ( string title ) : this(0, title)
+        {
+            //Initialize(title);
+            //Title = title;
+        }
+
+        public Movie ( int id, string title ) : this()
+        {
+            Id = id;
+            //Initialize(title);
+            Title = title;
+        }
+
+        //Shared init but dangerous to use as it can be called anywhere
+        //private void Initialize ( string title )
+        //{
+        //    Title = title;
+        //}
+        #endregion
+
+        //Mixed accessibility - one accessor may be more restrictive
+        //public int Id { get; private set; }
+        public int Id { get; } //private set ;
+
+        // Properties - methods with field-like syntax, exposes data
+        //   Property has a type and name
+        //   Properties are Pascal cased and generally public
+        //   Property has a getter to read the value and a setter to write the value
+        //   Getter must return a value of property type
+        //   Setter has a single parameter called `value` of property type
         public string Title
         {
-            // T get_Title()
-            get 
-            { 
-                // null coalescing ::= E ?? E (returns first non-null expresssion), changes the type of the expression
-                // works with anything that can be null
-                return _title ?? ""; 
-            }
-            // (_title != null) ? _title : ""; conditional operator
+            //null coalescing ::= E ?? E (returns first non-null expression)
+            //null conditional ::= E?.M (returns M?) changes the type of the expression
 
-            // Write void set_Title ( string value )
-            set 
-            {
-                // _title = (value != null) ? value.Trim() : null;
-                // if _title is null it skips .Trim and returns null
+            //Read: T get_Title ()
+            get {
+                return _title ?? "";
+
+                //return (_title != null) ? _title : "";
+                //if (_title == null)
+                //    return "";
+
+                //return _title; 
+            }
+
+            //Write void set_Title ( string value ) 
+            set {
+                //_title = value;
+                //_title = (value != null) ? value.Trim() : null;
                 _title = value?.Trim();
 
                 //Movie m;
-                //int id = m?.Id ?? 0; // int?
+                //int id = m?.Id ?? 0; //int?
             }
         }
-        /// <summary> gets and sets the description </summary>
+
+        /// <summary>Gets or sets the description.</summary>
         public string Description
         {
             get { return (_description != null) ? _description : ""; }
             set { _description = (value != null) ? value.Trim() : null; }
         }
+
         public string Rating
         {
             get { return (_rating != null) ? _rating : ""; }
             set { _rating = (value != null) ? value.Trim() : null; }
         }
-        // Full property Syntax
+
+        //Full property syntax
         //public int RunLength
-        //{            
-        //    get { return _runLength; }            
+        //{
+        //    get { return _runLength; }
         //    set { _runLength = value; }
         //}
-        public double ReviewRating { get; set; }
+        //Auto property
         public int RunLength { get; set; }
-        public int ReleaseYear { get; set; } = MinimumreleaseYear;
-        public bool IsClassic { get; set; }
-        
-        // TODO : Fix field casting, don't make public
-        // Fields
-        // 1. Always camel cased, TODO:for now
-        // 2. Should NEVER be public
-        // 3. Always zero initalized or can default
-        // 4. Cannot initalize to another field's value
 
+        //public int ReleaseYear
+        //{
+        //    get { return _releaseYear; }
+        //    set { _releaseYear = value; }
+        //}
+        /// <summary>Gets or sets the release year.</summary>
+        /// <value>MinimumReleaseYear</value>
+        public int ReleaseYear { get; set; } = MinimumReleaseYear;
+
+        //public double ReviewRating
+        //{
+        //    get { return _reviewRating; }
+        //    set { _reviewRating = value; }
+        //}
+        public double ReviewRating { get; set; }
+
+        public bool IsClassic { get; set; }
+        //{
+        //    get { return _isClassic; }
+        //    set { _isClassic = value; }
+        //}
+
+        //TODO: Fix field casing, don't make public
+        // Fields
+        //  1. Always camel cased
+        //  2. Should NEVER be public
+        //  3. Always zero initialized or can default
+        //  4. Cannot initialize to another field's value
         private string _title;
         private string _description;
         //private int _runLength;
-        private int _releaseYear = MinimumreleaseYear;
+
+        //TODO: Use the const Luke
+        //private int _releaseYear = MinimumReleaseYear;
+
         //private double _reviewRating;
         private string _rating;
         //private bool _isClassic;
 
         // Field is constant and therefore cannot be changed without recompiling
-        public const int MinimumreleaseYear = 1900;
+        public const int MinimumReleaseYear = 1900;
 
-        //public int GetAgeInYears()
+        //public int GetAgeInYears ()
         //{
-        //    return DateTime.Now.Year - _releaseYear;
+        //    return DateTime.Now.Year - ReleaseYear;
         //}
-        // calculated property
         public int AgeInYears
         {
-            get { return DateTime.Now.Year - _releaseYear; }
+            get { return DateTime.Now.Year - ReleaseYear; }
+            //set { }
         }
 
         //public bool IsBlackAndWhite ()
         //{
-        //    return _releaseYear <= 1922;
+        //    return ReleaseYear < 1922;
         //}
-
         public bool IsBlackAndWhite
         {
-            get { return _releaseYear <= 1922; }
+            get { return ReleaseYear < 1922; }
         }
 
-        // Methods - provide functionality (functions inside a class)
-        //          can refrence fields in method
+        // Methods - provide functionality (function inside a class)
+        //   Can reference fields in method
         //   `this` represents the current instance, always the first parameter (implied)
 
-        /// <summary> Copies the movie. </summary>
-        /// <returns> A copy of the movie. </returns>
-        public Movie Copy()
+        /// <summary>Copies the movie.</summary>
+        /// <returns>A copy of the movie.</returns>
+        public Movie Copy ()
         {
             var movie = new Movie();
             movie.Title = Title;
@@ -116,26 +178,27 @@ namespace MovieLibrary
 
         /// <summary>Validates the object.</summary>
         /// <returns>The error, if any.</returns>
-        public string Validate(/*Movie this*/)
+        public string Validate ( /* Movie this */ )
         {
-            // Name is required
-            if (String.IsNullOrEmpty(Title)) // this.title
+            //Title is required
+            if (String.IsNullOrEmpty(Title)) // this.title            
                 return "Title is required";
 
-            // Run Length  >= 0
+            //Run length >= 0
+            //if (this.runLength < 0)
             if (RunLength < 0)
                 return "Run Length must be at least zero";
 
-            // Realse year >= 1900
-            if (ReleaseYear < MinimumreleaseYear)
-                return "Release Year must be at least "+MinimumreleaseYear;
+            //Release year >= 1900            
+            if (ReleaseYear < MinimumReleaseYear)
+                return "Release Year must be at least " + MinimumReleaseYear;
 
             return null;
         }
 
         private void SetDescriptionToTitle ()
         {
-            _description = Title;
+            Description = Title;
         }
     }
 }
