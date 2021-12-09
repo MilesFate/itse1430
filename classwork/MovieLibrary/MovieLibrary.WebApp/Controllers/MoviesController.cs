0274d485eaceb5ee1b5edda87dd76b7fd056fa43
t2223239_s <t2223239_s@NEITD-110812>
2021-12-08 21:24:54 -0600

    ok now google can hire me, or amazon [ :

--- classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
@@ -21,5 +21,85 @@ namespace MovieLibrary.WebApp.Controllers
 
             return View(movies);
         }
+
+        [HttpGet]
+        public IActionResult Edit (int id)
+        {
+            var movie = _database.Get(id);
+            
+            if (movie == null)
+                return NotFound();
+            
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit (int id, Movie movie )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Update(id, movie);
+                    return RedirectToAction(nameof(List));
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("",e.Message);
+            };
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            var movie = new Movie();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Create ( Movie movie )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Add(movie);
+                    return RedirectToAction(nameof(List));
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            };
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Delete ( int id )
+        {
+            var movie = _database.Get(id);
+
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete ( int id, Movie movie )
+        {
+            try
+            {               
+                _database.Delete(id);
+                return RedirectToAction(nameof(List));
+                
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            };
+            return View(movie);
+        }
+
     }
 }
